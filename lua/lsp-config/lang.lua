e985f494f73e8d8fd6becfe2e3b1dd6abedf89bb
DDGRCF <raochaofan1970@gmail.com>
2024-02-26 10:50:11 +0800

    feat: format lang

--- lua/lsp-config/lang.lua
+++ lua/lsp-config/lang.lua
@@ -1,61 +1,53 @@
 local status_mason, mason = pcall(require, "mason")
 if not status_mason then
-  require("notify")("can't find mason.nvim")
-  return
+	require("notify")("can't find mason.nvim")
+	return
 end
 
 local status_lspconfig, lspconfig = pcall(require, "lspconfig")
 if not status_lspconfig then
-  require("notify")("can't find lspconfig")
-  return
+	require("notify")("can't find lspconfig")
+	return
 end
 
 local status_mason_lspconfig, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not status_mason_lspconfig then
-  require("notify")("can't find mason_lspconfig.nvim")
-  return
+	require("notify")("can't find mason_lspconfig.nvim")
+	return
 end
 
 mason.setup({
-  ui = {
-    border = "rounded",
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗"
-    }
-  },
-  log_level = vim.log.levels.INFO,
-  max_concurrent_installers = 4,
+	ui = {
+		border = "rounded",
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
 })
 
 local lsp_handlers = {
-  function(server_name)
-    lspconfig[server_name].setup {}
-  end,
-  ["lua_ls"] = function()
-    lspconfig.lua_ls.setup(
-      require("lsp-config.lang-config.lua_ls")
-    )
-  end,
-  ["clangd"] = function()
-    lspconfig.clangd.setup(
-      require("lsp-config.lang-config.clangd")
-    )
-  end,
-  ["pyright"] = function()
-    lspconfig.pyright.setup(
-      require("lsp-config.lang-config.pyright")
-    )
-  end,
-  ["gopls"] = function()
-    lspconfig.gopls.setup(
-      require("lsp-config.lang-config.gopls")
-    )
-  end,
-  ["cmake"] = function()
-    lspconfig.cmake.setup(require("lsp-config.lang-config.cmake"))
-  end,
+	function(server_name)
+		lspconfig[server_name].setup({})
+	end,
+	["lua_ls"] = function()
+		lspconfig.lua_ls.setup(require("lsp-config.lang-config.lua_ls"))
+	end,
+	["clangd"] = function()
+		lspconfig.clangd.setup(require("lsp-config.lang-config.clangd"))
+	end,
+	["pyright"] = function()
+		lspconfig.pyright.setup(require("lsp-config.lang-config.pyright"))
+	end,
+	["gopls"] = function()
+		lspconfig.gopls.setup(require("lsp-config.lang-config.gopls"))
+	end,
+	["cmake"] = function()
+		lspconfig.cmake.setup(require("lsp-config.lang-config.cmake"))
+	end,
 }
 
 mason_lspconfig.setup({
