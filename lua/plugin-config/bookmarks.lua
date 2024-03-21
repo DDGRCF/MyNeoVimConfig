4334a3e5e21f772af61da3bdf7f55f945217b372
DDGRCF <raochaofan1970@gmail.com>
2024-03-21 19:24:27 +0800

    feat: bookmarks icon

--- lua/plugin-config/bookmarks.lua
+++ lua/plugin-config/bookmarks.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_set_hl(0, "BookmarkSign", { link = "WarningMsg" })
 vim.api.nvim_set_hl(0, "BookmarkAnnotationSign", { link = "DiagnosticOk" })
 
-vim.g["bookmark_sign"] = ""
-vim.g["bookmark_annotation_sign"] = ""
+vim.g["bookmark_sign"] = ""
+vim.g["bookmark_annotation_sign"] = ""
 
 local status_telescope, telescope = pcall(require, "telescope")
 if status_telescope then
