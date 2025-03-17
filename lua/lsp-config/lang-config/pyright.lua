d4a5f8ffba042ef86cc14371d33b603641a62f11
r <DDGRCF>
2025-03-17 23:03:16 +0800

    feat: add pyright data ignore

--- lua/lsp-config/lang-config/pyright.lua
+++ lua/lsp-config/lang-config/pyright.lua
@@ -1,20 +1,25 @@
 local opts = {
-    filetypes = { "python" },
-    settings = {
-        pyright = {
-            disableOrganizeImports = true, -- Using Ruff
-        },
-        python = {
-            analysis = {
-                ignore = { '*' },                 -- Using Ruff
-                autoSearchPaths = true,
-                diagnosticMode = "openFilesOnly", -- workspace
-                useLibraryCodeForTypes = true,
-                typeCheckingMode = "off",
-            },
-        },
-    },
-    single_file_support = true,
+	filetypes = { "python" },
+	settings = {
+		pyright = {
+			disableOrganizeImports = true, -- Using Ruff
+		},
+		python = {
+			analysis = {
+				ignore = { "*" }, -- Using Ruff
+				autoSearchPaths = true,
+				diagnosticMode = "openFilesOnly", -- workspace
+				useLibraryCodeForTypes = true,
+				typeCheckingMode = "off",
+				exclude = {
+					"**/data/**",
+					"**/datasets/**",
+					"**/datasets/**",
+				},
+			},
+		},
+	},
+	single_file_support = true,
 }
 
 return opts
